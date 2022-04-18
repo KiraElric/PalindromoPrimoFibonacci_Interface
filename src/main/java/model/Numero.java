05297e80b8ea1f92981679fc41fb1dd2fdf4801e
KiraElric <lissette.urso@gmail.com>
2022-04-18 01:39:21 -0400

    Adding gitignored

--- src/main/java/model/Numero.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public abstract class Numero {
-    /*public abstract Boolean isPalindrome(int number);
-    public abstract Boolean isPrime(int number);
-    public abstract Boolean isFibbonacci(int number);*/
-    public abstract Boolean validate(int number);
-}
